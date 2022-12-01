508d1bfd42b9f7c6e3a0c326f2432604b4ccec19
skandog <muumrar@gmail.com>
2022-12-01 15:09:54 +0000

    trickier than it should have been to convert array of strings to array of nums"

--- index.js
+++ index.js
@@ -18,11 +18,25 @@ const input = `1000
 
 let sorted = input.split("\n\n");
 
-let totals = [];
+let groupedCals = [];
 
 for (let i = 0; i < sorted.length; i++) {
-    totals[i] = sorted[i].split('\n')
+  groupedCals[i] = sorted[i].split("\n");
 }
 
+let numsArr = [];
 
-console.log(totals);
+let totals = [];
+
+for (let j = 0; j < groupedCals.length; j++) {
+  numsArr[j] = groupedCals[j].map((str) => {
+    return parseInt(str);
+  });
+
+  totals[j] = groupedCals[j].reduce((total, num) => {
+    return total + num;
+  });
+}
+
+console.log(numsArr);
+// console.log(groupedCals);
